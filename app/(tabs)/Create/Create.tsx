99c50cb3feae41cd2f3b7c8a4bab33931bb92726
Grayidea-bit <lurenyi246891@gmail.com>
2024-06-10 23:19:57 +0800

    fix merge

--- app/(tabs)/Create/Create.tsx
+++ app/(tabs)/Create/Create.tsx
@@ -1,13 +1,15 @@
-import React, {useState,useEffect} from "react";
+import React, {useState,useEffect,useContext} from "react";
 import { Text, SafeAreaView, View, TextInput,Button } from "react-native";
 import json from "@constants/data/voc.json";
 import * as Fs from 'expo-file-system';
 import styles from "./Styles.ts";
 import { vocType } from "../Home/VocCard/VocCard.js";
 import { vocFileName } from "@/constants/fileName.ts";
+import { MyContext,myContextType } from "../_layout.ts";
 
 type Datatype = vocType; 
 const Create = () => {
+  const { reFetch, setReFetch } = useContext(MyContext) as myContextType;
   const [jsondata,Setjson] = useState<Datatype[]>([]);
   const [temp,Setdata] = useState<Datatype>(  {
     "word": "",
@@ -32,6 +34,8 @@ const SendData = async () => {
   const newData = {...temp,rememberValue:50}
   tempData.push(newData);
   await Fs.writeAsStringAsync(file,JSON.stringify(tempData));
+  setReFetch(!reFetch);
+  navigator.navigate()
 }
 const Header = () => {
   return (
