3599de0a3d5550e3973114706d021d8091be4cd8
mengxain0913 <mengxian0913@gmail.com>
2024-06-11 13:56:33 +0800

    setting theme color fontSize

--- app/(tabs)/Setting/Page/Display.tsx
+++ app/(tabs)/Setting/Page/Display.tsx
@@ -1,59 +1,158 @@
-import { Text, View, Switch, TouchableOpacity} from "react-native";
-import React, { useState, useRef } from "react";
+import { Text, View, Switch, TouchableOpacity } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
 import SelectDropdown from "react-native-select-dropdown";
 import SettingJson from "../Setting.json";
 import styles from "./Styles";
 import Colors from "@/constants/Colors";
 import { Entypo } from "@expo/vector-icons";
+import { screenHeight, screenWidth } from "@/constants/Config";
+import { isBlock } from "typescript";
+
+const myTextSize = {
+  Large: 28,
+  Medium: 22,
+  Small: 16,
+};
 
 const DisplayScreen = () => {
-  const [checked,SetChecked] = useState(SettingJson.Theme);
+  const [checked, SetChecked] = useState(SettingJson.Theme);
+  const [isBlack, setIsBlack] = useState<boolean>(true);
+  const [selector, setSelector] = useState("Medium");
+  const [ts, setTs] = useState(22);
   const Selector = useRef("Medium");
-  const textsize = [{title:"Large",value:"28"},{title:"Medium",value:"22"},{title:"Small",value:"16"}];
+  const textsize = [
+    { title: "Large", value: "28" },
+    { title: "Medium", value: "22" },
+    { title: "Small", value: "16" },
+  ];
   const ToggleSwitch = () => {
-    SetChecked(checked=>!checked);
-  }
+    SetChecked((checked) => !checked);
+    setIsBlack(!isBlack);
+  };
 
-  return(
-        <View style={styles.container}>
+  useEffect(() => {
+    const currT = selector;
+    console.log("currT: ", currT);
+
+    if (currT == "Large") {
+      setTs(28);
+    } else if (currT == "Medium") {
+      setTs(22);
+    } else {
+      setTs(16);
+    }
+  }, [selector]);
+
+  useEffect(() => {
+    console.log("ts: ", ts);
+  }, [ts]);
+
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        minHeight: screenHeight,
+        backgroundColor: !isBlack ? "black" : "#eee",
+      }}
+    >
       <View>
-        <TouchableOpacity style={styles.frequencySelector} onPress={ToggleSwitch}>
-        <Text style={styles.flattext}>Theme</Text>
-        <Entypo name="light-up" size={24} color="black" />
-        <Switch
+        <TouchableOpacity
           style={{
-          marginHorizontal:-20,
+            marginVertical: 20,
+            width: screenWidth * 0.9,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 20,
+            backgroundColor: !isBlack ? "lightgray" : "white",
+            borderRadius: 20,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 0.5 },
+            shadowOpacity: 0.2,
+            shadowRadius: 2,
           }}
-          value={checked}
-          onValueChange={ToggleSwitch}
-        />
-        <Entypo name="moon" size={24} color="black" />
+          onPress={ToggleSwitch}
+        >
+          <Text
+            style={{
+              color: Colors.light.text,
+              marginVertical: 25,
+              fontSize: ts,
+              fontWeight: "500",
+            }}
+          >
+            Theme
+          </Text>
+          <Entypo name="light-up" size={24} color="black" />
+          <Switch
+            style={{
+              marginHorizontal: -20,
+            }}
+            value={!checked}
+            onValueChange={ToggleSwitch}
+          />
+          <Entypo name="moon" size={24} color="black" />
         </TouchableOpacity>
-        <View style={styles.frequencySelector}>
-          <Text style={styles.flattext}>Text Size:</Text>
+        <View
+          style={{
+            marginVertical: 20,
+            width: screenWidth * 0.9,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 20,
+            backgroundColor: !isBlack ? "lightgray" : "white",
+            borderRadius: 20,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 0.5 },
+            shadowOpacity: 0.2,
+            shadowRadius: 2,
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.light.text,
+              marginVertical: 25,
+              fontSize: ts,
+              fontWeight: "500",
+            }}
+          >
+            Text Size:
+          </Text>
 
           <SelectDropdown
             data={textsize}
             onSelect={(selectedItem, index) => {
-            Selector.current = selectedItem.title;            
-            console.log(selectedItem, index);}}
+              setSelector(selectedItem.title);
+            }}
             renderButton={(selectedItem, isOpened) => {
-            return (
-            <View style={styles.dropdownButtonStyle}>
-              <Text style={styles.dropdownButtonTxtStyle}>
-                {(selectedItem && selectedItem.title) || SettingJson.TextFont}
-              </Text>
-            </View>);}}
+              return (
+                <View style={styles.dropdownButtonStyle}>
+                  <Text style={styles.dropdownButtonTxtStyle}>
+                    {(selectedItem && selectedItem.title) ||
+                      SettingJson.TextFont}
+                  </Text>
+                </View>
+              );
+            }}
             renderItem={(item, index, isSelected) => {
-            return (
-            <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
-              <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
-            </View>);}}
+              return (
+                <View
+                  style={{
+                    ...styles.dropdownItemStyle,
+                    ...(isSelected && { backgroundColor: "#D2D9DF" }),
+                  }}
+                >
+                  <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+                </View>
+              );
+            }}
             showsVerticalScrollIndicator={false}
-            dropdownStyle={styles.dropdownMenuStyle} /> 
+            dropdownStyle={styles.dropdownMenuStyle}
+          />
         </View>
       </View>
     </View>
-  )
+  );
 };
 export default DisplayScreen;
